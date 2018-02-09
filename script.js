b8e8aaea558145c40c13eb68b2c9ca8f21065341
Paige Power <paige.power2@gmail.com>
2018-02-09 12:25:55 -0500

    background colour change random

---
+++ script.js
@@ -0,0 +1,58 @@
+// pseudocode here
+
+// user has option to change color palette to own choice or one that is already provided
+// choices of palette will be ones I made myself on color hunt
+
+// on click figure out hover on buttons
+// figure out how to change body colour
+// colours stores in array
+// return random number from array
+// can randomize one color at a time or all
+// switch button so user can switch colours around on element
+
+// the user inputs colour palette choices
+// user can change the hex colours of the elements on the page
+
+// generate custom style guide for user 
+
+
+
+// HEX COLOUR RANDOMIZER
+// formula
+// '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+
+
+// SAVE COLOUR COMBINATIONS
+
+// COLOUR COMBO LIBRARY
+
+
+
+
+
+// global scope
+// empty object {}
+// main container
+const myApp = {};
+
+// do not need to preface with const 
+// array of elements in variable
+
+
+// background colour changes to random HEX colour
+$('.background').css('background-color', '#' + (Math.random() * 0xFFFFFF << 0).toString(16));
+
+// events
+// on click return random number for each element, background, title, paragraph and icons
+// on click return random number for only one element at a time
+
+
+
+
+// on click allow user to input HEX value
+
+
+
+
+
+
